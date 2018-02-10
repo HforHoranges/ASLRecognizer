87ac4a85175daa928bae035eedfc0a10840850dd
hilal <hasmat@sfu.ca>
2018-02-10 14:18:23 -0800

    Added skeleton video display code

---
+++ app/src/main/java/hforh/oranges/aslrecognizer/ASLDisplayActivity.java
@@ -0,0 +1,17 @@
+package hforh.oranges.aslrecognizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ASLDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asldisplay);
+    }
+
+    public void DisplayVideo(String url){
+
+    }
+}
