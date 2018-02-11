68a4ef014f246f12b2e1c61af02d03744ce17b63
hilal <hasmat@sfu.ca>
2018-02-11 05:53:35 -0800

    Have a new video display activity tht uses YouTube's API

--- app/src/main/java/hforh/oranges/aslrecognizer/ASLDisplayActivity.java
+++ app/src/main/java/hforh/oranges/aslrecognizer/ASLDisplayActivity.java
@@ -1,71 +1,141 @@
 package hforh.oranges.aslrecognizer;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class ASLDisplayActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-    private String youtubeVideoStringBeforeURL ="<html><center><body><iframe width=\"420\" height=\"315\" src=\"";
-    private String youtubeVideoStringAfterURL = "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-    private String youtubeVideoURL = "https://www.youtube.com/embed/zX1q1NLwznY";
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+public class ASLDisplayActivity extends YouTubeFailureRecoveryActivity implements
+        View.OnClickListener,
+        CompoundButton.OnCheckedChangeListener,
+        YouTubePlayer.OnFullscreenListener{
+    private static final int PORTRAIT_ORIENTATION = Build.VERSION.SDK_INT < 9
+            ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            : ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT;
+
+    private LinearLayout baseLayout;
+    private YouTubePlayerView playerView;
+    private YouTubePlayer player;
+    private View otherViews;
+
+    private boolean fullscreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asldisplay);
+        baseLayout = (LinearLayout) findViewById(R.id.layout);
+        playerView = (YouTubePlayerView) findViewById(R.id.player);
+        otherViews = findViewById(R.id.other_views);
+
+        playerView.initialize(DeveloperKey.DEVELOPER_KEY, this);
+
+        doLayout();
     }
 
-    public void DisplayVideo(String url){
-        if (url.contains("youtube")){
-            DisplayYoutubeVideo(url);
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return playerView;
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        this.player = youTubePlayer;
+        // Specify that we want to handle fullscreen behavior ourselves.
+        player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
+        player.setOnFullscreenListener(this);
+        if (!wasRestored) {
+            player.loadVideo("avP5d16wEp0");
         }
-        else{
-            DisplayRegularVideo(url);
+    }
+
+    private void doLayout() {
+        LinearLayout.LayoutParams playerParams =
+                (LinearLayout.LayoutParams) playerView.getLayoutParams();
+        if (fullscreen) {
+            // When in fullscreen, the visibility of all other views than the player should be set to
+            // GONE and the player should be laid out across the whole screen.
+            playerParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+            playerParams.height = LinearLayout.LayoutParams.MATCH_PARENT;
+
+            otherViews.setVisibility(View.GONE);
+        } else {
+            // This layout is up to you - this is just a simple example (vertically stacked boxes in
+            // portrait, horizontally stacked in landscape).
+            otherViews.setVisibility(View.VISIBLE);
+            ViewGroup.LayoutParams otherViewsParams = otherViews.getLayoutParams();
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                playerParams.width = otherViewsParams.width = 0;
+                playerParams.height = WRAP_CONTENT;
+                otherViewsParams.height = MATCH_PARENT;
+                playerParams.weight = 1;
+                baseLayout.setOrientation(LinearLayout.HORIZONTAL);
+            } else {
+                playerParams.width = otherViewsParams.width = MATCH_PARENT;
+                playerParams.height = WRAP_CONTENT;
+                playerParams.weight = 0;
+                otherViewsParams.height = 0;
+                baseLayout.setOrientation(LinearLayout.VERTICAL);
+            }
         }
     }
 
-    private void DisplayRegularVideo(String url) {
-        String path = "https://www.youtube.com/watch?v=RlIdWGOVfTQ";
-        Uri uri = Uri.parse(path);
-        final VideoView vv = (VideoView) findViewById(R.id.videoView3);
-        // Start the MediaController
-        MediaController mediacontroller = new MediaController(this);
-        mediacontroller.setAnchorView(vv);
-        vv.setMediaController(mediacontroller);
-        vv.setVideoURI(uri);
-        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            public void onPrepared(MediaPlayer mp) {
-                mp.setLooping(true);
-                vv.start();
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        player.setFullscreen(!fullscreen);
     }
 
-    private void DisplayYoutubeVideo(String url) {
-        String frameVideo = youtubeVideoStringBeforeURL + url + youtubeVideoStringAfterURL;
-        Log.d("ASLDISP", frameVideo);
-        Log.d("ASLDISP", "<html><center><body><iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/47yJ2XCRLZs\" frameborder=\"0\" allowfullscreen></iframe></body></html>");
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        /*int controlFlags = player.getFullscreenControlFlags();
+        if (isChecked) {
+            // If you use the FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE, your activity's normal UI
+            // should never be laid out in landscape mode (since the video will be fullscreen whenever the
+            // activity is in landscape orientation). Therefore you should set the activity's requested
+            // orientation to portrait. Typically you would do this in your AndroidManifest.xml, we do it
+            // programmatically here since this activity demos fullscreen behavior both with and without
+            // this flag).
+            setRequestedOrientation(PORTRAIT_ORIENTATION);
+            controlFlags |= YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE;
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+            controlFlags &= ~YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE;
+        }
+        player.setFullscreenControlFlags(controlFlags);*/
+    }
 
-        WebView displayYoutubeVideo = (WebView) findViewById(R.id.youtubeWebView);
-        displayYoutubeVideo . setWebViewClient ( new  WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        WebSettings webSettings = displayYoutubeVideo . getSettings ();
-        webSettings.setJavaScriptEnabled(true);
-        displayYoutubeVideo.loadData(frameVideo, "textToTranslate/html", "utf-8");
+    @Override
+    public void onFullscreen(boolean isFullscreen) {
+        fullscreen = isFullscreen;
+        doLayout();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        doLayout();
     }
 }
