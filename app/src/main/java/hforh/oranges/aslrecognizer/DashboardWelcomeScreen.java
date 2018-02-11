654e5ca46772b999dcd6d0cb44145ecb29d6152f
hilal <hasmat@sfu.ca>
2018-02-10 23:51:29 -0800

    Extended Activity instead of AppCompatActivity

--- app/src/main/java/hforh/oranges/aslrecognizer/DashboardWelcomeScreen.java
+++ app/src/main/java/hforh/oranges/aslrecognizer/DashboardWelcomeScreen.java
@@ -1,12 +1,13 @@
 package hforh.oranges.aslrecognizer;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DashboardWelcomeScreen extends AppCompatActivity {
+public class DashboardWelcomeScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
