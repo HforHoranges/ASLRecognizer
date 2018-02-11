763e7b80619a6a8171123bb509abc1bfd05f4788
p8e1b <p8e1b@ugrad.cs.ubc.ca>
2018-02-10 16:46:52 -0800

    edit-text branch-2: has working click and edit function for edit text

--- app/src/main/java/hforh/oranges/aslrecognizer/EditTextActivity.java
+++ app/src/main/java/hforh/oranges/aslrecognizer/EditTextActivity.java
@@ -2,12 +2,34 @@ package hforh.oranges.aslrecognizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditTextActivity extends AppCompatActivity {
+
+    public String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
+    }
+
+       protected void populateText() {
+        EditText initialText = (EditText) findViewById(R.id.textBox);
+        initialText.setText(text);
+
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean doneEdit = ((CheckBox) view).isChecked();
+        EditText editedText = (EditText) findViewById(R.id.textBox);
+        if (doneEdit) {
+            editedText.getText().toString(); // feed this into translator
+        }
     }
 }
