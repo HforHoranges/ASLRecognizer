3fa34b2351505a7101db522d4694c49303485267
hilal <hasmat@sfu.ca>
2018-02-10 17:24:54 -0800

    Final push on this branch for now

--- app/src/main/java/hforh/oranges/aslrecognizer/OcrCaptureActivity.java
+++ app/src/main/java/hforh/oranges/aslrecognizer/OcrCaptureActivity.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
@@ -49,7 +50,11 @@ import hforh.oranges.aslrecognizer.ui.camera.GraphicOverlay;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Locale;
 
 /**
